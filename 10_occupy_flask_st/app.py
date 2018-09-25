f2dfc84d83c910bf74be4a0f011a29f94e1ffc06
jabir chowdhury <jchowdhury@stuy.edu>
2018-09-24 22:13:51 -0400

    Revised work

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -4,17 +4,17 @@
 #2018-09-23
 
 from flask import Flask,render_template
-from csvscript import make_dictionary,random_job
+from csvscript import make_dictionary,random_job #functions from previous work
 
 app = Flask(__name__)
 
 
 @app.route("/occupations")
 def occupation_table():
-    occu_dict = make_dictionary("data/occupations.csv")
+    occu_dict = make_dictionary("data/occupations.csv")  #makes a dictionary using occupations.csv
     return render_template("chart.html",
-                           dict = occu_dict,
-                           rand= random_job(occu_dict))
+                           dict = occu_dict,   #passes the dictionary to a variable used in jinja
+                           rand= random_job(occu_dict))  #passes a random weighted occupation from dictionary
 
 if __name__ == "__main__":
   app.debug = True
