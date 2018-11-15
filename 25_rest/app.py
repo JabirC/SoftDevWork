d212c5371fc5eab3b6fb0f28307aabe00cc24296
jabir chowdhury <jchowdhury@stuy.edu>
2018-11-15 02:23:06 -0500

    added work for houndify api

---
+++ 25_rest/app.py
@@ -0,0 +1,29 @@
+import json
+
+from flask import Flask, render_template , request
+import houndify
+
+app = Flask(__name__)
+clientId = "MsX70vzsQjNbSx31FX87OQ=="
+clientKey = "0mk1n0mBX_q9nXSlabXe8caqmAh1J_pohbCV5rLeMwkO8ccImxN6Z8BTPZ6U5E2vTv6C93AIH4eeSTMu7h2zqw=="
+userId = "Admin"
+
+
+@app.route("/")
+def init():
+    return render_template("site.html")
+
+@app.route("/results" , methods=["POST"])
+def searchresults():
+    client = houndify.TextHoundClient(clientId, clientKey, userId)
+    response = client.query(request.form.get("query"))
+    listresults = response["AllResults"]
+    dictresults = listresults[0]
+    dictresults = dictresults["HTMLData"]
+    stringResults = dictresults["LargeScreenHTML"]
+    return render_template("results.html" , htmlcode= stringResults )
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
